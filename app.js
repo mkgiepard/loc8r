e7ecc2d2d9a5781147184cb6b4df99460296f70e
mkgiepard <mkgiepard@gmail.com>
2019-09-01 18:40:31 +0200

    mean: adding mongoose connection handling

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const favicon = require('serve-favicon');
+require('./app_server/models/db');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
