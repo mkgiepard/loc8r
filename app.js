62709e6de187a008875d4f17db8a19d3c22caad9
mkgiepard <mkgiepard@gmail.com>
2019-09-08 13:32:48 +0200

    mean: adding REST API

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const favicon = require('serve-favicon');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 const indexRouter = require('./app_server/routes/index');
-const usersRouter = require('./app_server/routes/users');
+const apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
