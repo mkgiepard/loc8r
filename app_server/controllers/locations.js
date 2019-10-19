58d49bee9145c13cbb451e85bdd709bdab6442f9
mkgiepard <mkgiepard@gmail.com>
2019-10-19 17:29:51 +0200

    mean: connecting RESP API with location info page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -57,47 +57,42 @@ const homelist = (req, res) => {
 };
 
 /* GET 'Location info' page */
+const renderDetailPage = (req, res, location) => {
+  res.render('location-info', {
+    title: location.name,
+    pageHeader: {
+      title: location.name
+    },
+    sidebar: {
+      context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location
+  });
+}
+
 const locationInfo = (req, res) => {
-    res.render('location-info', {
-      title: 'Starcups',
-      pageHeader: {title: 'Starcups'},
-      sidebar: {
-        context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-        callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
-      },
-      location: {
-        name: 'Starcups',
-        address: '125 High Street, Reading, RG6 1PS',
-        rating: 3,
-        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-        coords: {lat: 52.392880, lng: 16.892780},
-        openingTimes: [{
-          days: 'Monday - Friday',
-          opening: '7:00am',
-          closing: '7:00pm',
-          closed: false
-        },{
-          days: 'Saturday',
-          opening: '8:00am',
-          closing: '5:00pm',
-          closed: false
-        },{
-          days: 'Sunday',
-          closed: true
-        }],
-        reviews: [{
-          author: 'Simon Holmes',
-          rating: 5,
-          timestamp: '16 July 2013',
-          reviewText: 'What a great place. I can\'t say enough good things about it.'
-        },{
-          author: 'Charlie Chaplin',
-          rating: 3,
-          timestamp: '16 June 2013',
-          reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-        }]
+  const path = `/api/locations/${req.params.locationid}`;
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {}
+  };
+  request(
+    requestOptions,
+    (err, {statusCode}, body) => {
+      const data = body;
+      if (statusCode === 200) {
+        data.coords = {
+          lat: body.coords[0],
+          lng: body.coords[1]
+        }
+        renderDetailPage(req, res, data);
+      } else {
+        showError(req, res, statusCode);
       }
-    });
+    }
+  )
 };
 
 /* GET 'Add review' page */
@@ -118,6 +113,23 @@ const formatDistance = (distance) => {
     thisDistance = Math.floor(distance);
   }
   return thisDistance + unit;
+}
+
+const showError = (req, res, status) => {
+  let title = '';
+  let content = '';
+  if (status === 404) {
+    title = '404, page not found';
+    content = 'Oh dear. Looks like you can\'t find this page. Sorry.';
+  } else {
+    title = `${status}, something's gone wrong`;
+    content = 'Something, somewhere, has gone just a little bit wrong.';
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title,
+    content
+  });
 }
 
 module.exports = {
