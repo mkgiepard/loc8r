465672c5889018b8a70ebd7f26dcfc63f18cb0e9
mkgiepard <mkgiepard@gmail.com>
2019-10-20 16:53:48 +0200

    mean: adding angular service to pull the data

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Loc8rDataService } from '../loc8r-data.service';
 
 export class Location {
   _id: string;
@@ -16,24 +17,18 @@ export class Location {
 })
 export class HomeListComponent implements OnInit {
 
-  constructor() { }
-  locations: Location[] = [{
-    _id: '5d6bfbb5e43a39116fb877d5',
-    name: 'xStarsups',
-    distance: 12114.6,
-    address: 'x125 High Street, Reading, RG6 1PS',
-    rating: 3,
-    facilities: ['Hot drinks', 'Food', 'Power', 'WiFi']
-  }, {
-    _id: '5d6bfbb5e43a39116fb87123',
-    name: 'xCosty',
-    distance: 92114.6,
-    address: 'x126 High Street, Reading, RG6 1PS',
-    rating: 4,
-    facilities: ['Hot drinks', 'Food', 'WiFi']
-  }];
+  constructor(private loc8rDataService: Loc8rDataService) { }
+
+  public locations: Location[];
+
+  private getLocations(): void {
+    this.loc8rDataService
+      .getLocations()
+        .then(foundLocations => this.locations = foundLocations);
+  }
 
   ngOnInit() {
+    this.getLocations();
   }
 
 }
